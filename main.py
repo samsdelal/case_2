eb72e21dd7d0b0e78fe3696d871a33dfc8c3557f
Boris Kuznecov <oralebot@mail.ru>
2019-10-08 11:13:55 +0700

    y

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#hello
+# string constants
+"""name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
+
+annual_income = 0
+for month in range(12):
+    print('{} {}:'.format(QUESTION,name_month[month], end ='')
+    income = float(input())
+    annual_income += income
+print(annual_income)"""
+
+
+
