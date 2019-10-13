d5206086946b05f854085cc25cd6aef2adb1879f
Boris Kuznecov <oralebot@mail.ru>
2019-10-13 17:41:12 +0700

    incomes

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # string constants
+"""Case-study #0 My first code
+Developers:
+Makarov A (--%), Kuznecov B(30%), Odoevcev S(50%) 
+
+
+"""
 """name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
 
 annual_income = 0
@@ -8,10 +14,13 @@ for month in range(12):
     annual_income += income
 print(annual_income)"""
 
-family = input('Выберите  семейный статус налогоплатильщика\n'
-               '\n'
-               '1.[Один субъект]\n'
-               '2.[Супружеская пара]\n'
-               '3.[Родитель-одиночка]')
+family = input("Выберите  семейный статус налогоплатильщика\n"
+               "\n"
+               "1.[Один субъект]\n"
+               "2.[Супружеская пара]\n"
+               "3.[Родитель-одиночка]\n"
+               "---")
+print('---------------------------')
+incomes = int(input("Введите свой доход --- "))
 
 
